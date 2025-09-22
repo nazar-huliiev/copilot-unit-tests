fe6c4d31e67110d79d1f5b4cc3f135f4087082b7
Nazar Huliiev <nazar.huliev@gmail.com>
2025-09-22 15:05:37 +0300

    Init projects

---
+++ CopilotUnitTests.Api.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace CopilotUnitTests.Api.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var result = 2 + 2;
+
+            Assert.Equal(4, result);
+        }
+    }
+}
